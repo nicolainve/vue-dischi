fa7245e16ac02b0264fafb4edd56f0fb3ee9d40a
Nicola <nginvernizzi1@gmail.com>
2020-11-24 16:29:35 +0100

    filter done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const app = new Vue({
     el:'#app',
     data: {
-        cds: []
+        cds: [],
+        actualGenre: 'all'
     },
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
@@ -11,5 +12,24 @@ const app = new Vue({
         .catch(error => {
             console.log(error);
         })
+    },
+    methods: {
+        filterGenre() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(response => {
+                let cdsList = response.data.response;
+
+                if (this.actualGenre !== 'all') {
+                    cdsList = cdsList.filter(element => {
+                        return element.genre.toLowerCase() === this.actualGenre
+                    });
+                }
+
+                this.cds = cdsList;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
     }
 });
